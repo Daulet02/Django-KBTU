912667fcda183b4cc35678e12cd5f5e3d9386288
Daulet_K <daulet_kd01@mail.ru>
2021-03-06 10:42:41 +0600

    django

--- Lab_4/main/admin.py
+++ Lab_4/main/admin.py
@@ -7,12 +7,8 @@ from main.models import Todo_list, Task
 @admin.register(Todo_list)
 class Todo_list_admin(admin.ModelAdmin):
     list_display = ['list_name']
-    ordering = ['list_name']
 
 
 @admin.register(Task)
 class Task_admin(admin.ModelAdmin):
     list_display = ['task_name', 'created', 'due_on', 'owner', 'todo_list']
-    ordering = ['task_name']
-    search_fields = ['task_name', 'owner__first_name', 'todo_list__list_name', ]
-    list_filter = ['created', 'due_on']
